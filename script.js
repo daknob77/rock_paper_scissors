3bbd18aaefe3a8b1cfa9feba5e55f0e4feed731e
daknob77 <121295353+daknob77@users.noreply.github.com>
2023-11-24 21:25:26 +0800

    Add variable names to relevant elements

---
+++ script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const rockBtn = document.getElementById("rock");
+const paperBtn = document.getElementById("paper");
+const scissorsBtn = document.getElementById("scissors");
+
+const playerIcon = document.getElementsByClassName("player-icon");
+const compIcon = document.getElementsByClassName("computer-icon");
+
+const playerScore = document.getElementById("player-score");
+const compScore = document.getElementById("computer-score");
+
+const status = document.getElementsByClassName("rule");
