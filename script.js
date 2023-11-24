120bb9e2652c8c5b3c226291956ff82d242be0bf
daknob77 <121295353+daknob77@users.noreply.github.com>
2023-11-24 23:55:00 +0800

    Add functions for game functionality Add gameMech function for rock paper scissors function Add Rock Paper Scissors btn event listeners for player choice Add boolGame function to limit game to 5 points Add changeImg function to change player and computer choice image

--- script.js
+++ script.js
@@ -4,10 +4,137 @@ const rockBtn = document.getElementById("rock");
 const paperBtn = document.getElementById("paper");
 const scissorsBtn = document.getElementById("scissors");
 
-const playerIcon = document.getElementsByClassName("player-icon");
-const compIcon = document.getElementsByClassName("computer-icon");
+let playerIcon = document.getElementsByClassName("player-icon");
+let compIcon = document.getElementsByClassName("computer-icon");
 
-const playerScore = document.getElementById("player-score");
-const compScore = document.getElementById("computer-score");
+let playerScore = document.getElementById("player-score");
+let compScore = document.querySelector("#computer-score");
+let player = 0;
+let comp = 0;
 
-const status = document.getElementsByClassName("rule");
+let gameStatus = document.querySelector(".rule");
+let roundStatus = document.querySelector(".choice");
+
+const choices = ["rock", "paper", "scissors"];
+let playerChoice = "";
+let computerChoice = "";
+let playing = true;
+
+function init() {
+  playing = true;
+  player = 0;
+  comp = 0;
+}
+
+function gameMech(playerChoice, compChoice) {
+  if (player === 5) {
+    boolGame();
+    return;
+  } else if (comp === 5) {
+    boolGame();
+    return;
+  } else {
+    switch (playerChoice) {
+      case "rock":
+        switch (compChoice) {
+          case "rock":
+            roundStatus.textContent = "TIE";
+            break;
+          case "paper":
+            roundStatus.textContent = "YOU LOSE";
+            comp += 1;
+            compScore.textContent = comp;
+            break;
+          case "scissors":
+            roundStatus.textContent = "YOU WIN";
+            player += 1;
+            playerScore.textContent = player;
+            break;
+        }
+        break;
+      case "paper":
+        switch (compChoice) {
+          case "rock":
+            roundStatus.textContent = "YOU WIN";
+            player += 1;
+            playerScore.textContent = player;
+            break;
+          case "paper":
+            roundStatus.textContent = "TIE";
+            break;
+          case "scissors":
+            roundStatus.textContent = "YOU LOSE";
+            comp += 1;
+            compScore.textContent = comp;
+            break;
+        }
+        break;
+      case "scissors":
+        switch (compChoice) {
+          case "rock":
+            roundStatus.textContent = "YOU LOSE";
+            comp += 1;
+            compScore.textContent = comp;
+            break;
+          case "paper":
+            roundStatus.textContent = "YOU WIN";
+            player += 1;
+            playerScore.textContent = player;
+            break;
+          case "scissors":
+            roundStatus.textContent = "TIE";
+            break;
+        }
+        break;
+    }
+  }
+}
+
+rockBtn.addEventListener("click", function () {
+  if (playing) {
+    playerChoice = choices[0];
+
+    computerChoice = choices[Math.trunc(Math.random() * 3)];
+    changeImg(playerChoice, computerChoice);
+
+    gameMech(playerChoice, computerChoice);
+  }
+});
+
+paperBtn.addEventListener("click", function () {
+  if (playing) {
+    playerChoice = choices[1];
+
+    computerChoice = choices[Math.trunc(Math.random() * 3)];
+    changeImg(playerChoice, computerChoice);
+
+    gameMech(playerChoice, computerChoice);
+  }
+});
+
+scissorsBtn.addEventListener("click", function () {
+  if (playing) {
+    playerChoice = choices[2];
+
+    computerChoice = choices[Math.trunc(Math.random() * 3)];
+    changeImg(playerChoice, computerChoice);
+
+    gameMech(playerChoice, computerChoice);
+  }
+});
+
+function boolGame(bool) {
+  playing = false;
+  if (bool) {
+    gameStatus.textContent = "YOU HAVE WON!!!";
+  } else {
+    gameStatus.textContent = "YOU LOST";
+  }
+}
+
+function changeImg(playerChoice, computerChoice) {
+  document.querySelector(".player-icon").src = `./assets/${playerChoice}.svg`;
+  document.querySelector(
+    ".computer-icon"
+  ).src = `./assets/${computerChoice}.svg`;
+}
