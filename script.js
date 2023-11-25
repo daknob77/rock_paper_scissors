1b80c40b2ae29135ae5fa5913e202e728b2dcd55
daknob77 <121295353+daknob77@users.noreply.github.com>
2023-11-25 09:40:32 +0800

    Change textContent of roundStatus. Change score variable to score array. Add game init function. Add event listener to new game button with game init function. Add function to produce random number from 1 to 3 for computerChoice

--- script.js
+++ script.js
@@ -3,14 +3,10 @@
 const rockBtn = document.getElementById("rock");
 const paperBtn = document.getElementById("paper");
 const scissorsBtn = document.getElementById("scissors");
-
-let playerIcon = document.getElementsByClassName("player-icon");
-let compIcon = document.getElementsByClassName("computer-icon");
+const initBtn = document.querySelector(".init");
 
 let playerScore = document.getElementById("player-score");
 let compScore = document.querySelector("#computer-score");
-let player = 0;
-let comp = 0;
 
 let gameStatus = document.querySelector(".rule");
 let roundStatus = document.querySelector(".choice");
@@ -19,84 +15,94 @@ const choices = ["rock", "paper", "scissors"];
 let playerChoice = "";
 let computerChoice = "";
 let playing = true;
+let scores = [0, 0];
 
 function init() {
   playing = true;
-  player = 0;
-  comp = 0;
+  scores = [0, 0];
+  compScore.textContent = scores[0];
+  playerScore.textContent = scores[1];
+
+  roundStatus.textContent = "Rock Paper or Scissors?";
+  gameStatus.textContent = "First to get 5 points wins";
+  document.querySelector(".player-icon").src = `./assets/question-mark.svg`;
+  document.querySelector(".computer-icon").src = `./assets/question-mark.svg`;
 }
 
 function gameMech(playerChoice, compChoice) {
-  if (player === 5) {
-    boolGame();
+  switch (playerChoice) {
+    case "rock":
+      switch (compChoice) {
+        case "rock":
+          roundStatus.textContent = "TIE";
+          break;
+        case "paper":
+          roundStatus.textContent = "YOU LOSE THE ROUND";
+          scores[1] += 1;
+          compScore.textContent = scores[1];
+          break;
+        case "scissors":
+          roundStatus.textContent = "YOU WIN THE ROUND";
+          scores[0] += 1;
+          playerScore.textContent = scores[0];
+          break;
+      }
+      break;
+    case "paper":
+      switch (compChoice) {
+        case "rock":
+          roundStatus.textContent = "YOU WIN THE ROUND";
+          scores[0] += 1;
+          playerScore.textContent = scores[0];
+          break;
+        case "paper":
+          roundStatus.textContent = "TIE";
+          break;
+        case "scissors":
+          roundStatus.textContent = "YOU LOSE THE ROUND";
+          scores[1] += 1;
+          compScore.textContent = scores[1];
+          break;
+      }
+      break;
+    case "scissors":
+      switch (compChoice) {
+        case "rock":
+          roundStatus.textContent = "YOU LOSE THE ROUND";
+          scores[1] += 1;
+          compScore.textContent = scores[1];
+          break;
+        case "paper":
+          roundStatus.textContent = "YOU WIN THE ROUND";
+          scores[0] += 1;
+          playerScore.textContent = scores[0];
+          break;
+        case "scissors":
+          roundStatus.textContent = "TIE";
+          break;
+      }
+      break;
+  }
+  boolGame(scores);
+}
+
+function boolGame(scores) {
+  if (scores[0] === 5) {
+    gameStatus.textContent = "YOU WON THE GAME!!!";
+    playing = false;
     return;
-  } else if (comp === 5) {
-    boolGame();
+  } else if (scores[1] === 5) {
+    gameStatus.textContent = "YOU LOST THE GAME";
+    playing = false;
     return;
-  } else {
-    switch (playerChoice) {
-      case "rock":
-        switch (compChoice) {
-          case "rock":
-            roundStatus.textContent = "TIE";
-            break;
-          case "paper":
-            roundStatus.textContent = "YOU LOSE";
-            comp += 1;
-            compScore.textContent = comp;
-            break;
-          case "scissors":
-            roundStatus.textContent = "YOU WIN";
-            player += 1;
-            playerScore.textContent = player;
-            break;
-        }
-        break;
-      case "paper":
-        switch (compChoice) {
-          case "rock":
-            roundStatus.textContent = "YOU WIN";
-            player += 1;
-            playerScore.textContent = player;
-            break;
-          case "paper":
-            roundStatus.textContent = "TIE";
-            break;
-          case "scissors":
-            roundStatus.textContent = "YOU LOSE";
-            comp += 1;
-            compScore.textContent = comp;
-            break;
-        }
-        break;
-      case "scissors":
-        switch (compChoice) {
-          case "rock":
-            roundStatus.textContent = "YOU LOSE";
-            comp += 1;
-            compScore.textContent = comp;
-            break;
-          case "paper":
-            roundStatus.textContent = "YOU WIN";
-            player += 1;
-            playerScore.textContent = player;
-            break;
-          case "scissors":
-            roundStatus.textContent = "TIE";
-            break;
-        }
-        break;
-    }
   }
 }
 
 rockBtn.addEventListener("click", function () {
   if (playing) {
     playerChoice = choices[0];
-
-    computerChoice = choices[Math.trunc(Math.random() * 3)];
+    computerChoice = choices[rand1To3()];
     changeImg(playerChoice, computerChoice);
-
     gameMech(playerChoice, computerChoice);
   }
 });
@@ -104,10 +110,8 @@ rockBtn.addEventListener("click", function () {
 paperBtn.addEventListener("click", function () {
   if (playing) {
     playerChoice = choices[1];
-
-    computerChoice = choices[Math.trunc(Math.random() * 3)];
+    computerChoice = choices[rand1To3()];
     changeImg(playerChoice, computerChoice);
-
     gameMech(playerChoice, computerChoice);
   }
 });
@@ -115,22 +119,11 @@ paperBtn.addEventListener("click", function () {
 scissorsBtn.addEventListener("click", function () {
   if (playing) {
     playerChoice = choices[2];
-
-    computerChoice = choices[Math.trunc(Math.random() * 3)];
+    computerChoice = choices[rand1To3()];
     changeImg(playerChoice, computerChoice);
-
     gameMech(playerChoice, computerChoice);
   }
 });
-
-function boolGame(bool) {
-  playing = false;
-  if (bool) {
-    gameStatus.textContent = "YOU HAVE WON!!!";
-  } else {
-    gameStatus.textContent = "YOU LOST";
-  }
-}
 
 function changeImg(playerChoice, computerChoice) {
   document.querySelector(".player-icon").src = `./assets/${playerChoice}.svg`;
@@ -138,3 +131,9 @@ function changeImg(playerChoice, computerChoice) {
     ".computer-icon"
   ).src = `./assets/${computerChoice}.svg`;
 }
+
+const rand1To3 = function () {
+  return Math.trunc(Math.random() * 3);
+};
+
+initBtn.addEventListener("click", init);
